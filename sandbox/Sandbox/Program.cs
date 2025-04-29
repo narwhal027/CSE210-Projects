315d627eeab56f5a7e8fb80950756eb496532e65
Logan Hansen <Logan.hansen.027@gmail.com>
2025-04-29 09:36:26 -0600

    changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        int x = 10;
+        if (x == 10)
+        {
+            Console.WriteLine("X is 10");
+        }
+        for (int i = 0; i < x; i++)
+        {
+            Console.WriteLine($"Logan is cool: {i * i}.");
+        }
     }
 }
