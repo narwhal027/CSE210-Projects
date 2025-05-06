58e318046650afaed4c69608c691887bdc119278
Logan Hansen <Logan.hansen.027@gmail.com>
2025-05-06 09:58:39 -0600

    objects and classes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int numGuess = 0;
+        Random rand = new Random();
+        int randNum = rand.Next(1, 100);
+        while (numGuess != randNum)
+        {
+            Console.WriteLine("Guess the magic number: ");
+            numGuess = int.Parse(Console.ReadLine());
+            if (numGuess == randNum)
+            {
+                Console.WriteLine("You guessed the number, congratulations!");
+            }
+            else if (numGuess > randNum)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("Too low.");
+            }
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,19 +2,53 @@ using System;
 using System.Security.Cryptography.X509Certificates;
 // This is a comment.
 // Another comment.
+class Circle
+{
+    private double _radius;
+
+    public void SetRadius(double radius)
+    {
+        if (radius < 0)
+        {
+            Console.WriteLine("Error");
+            return;
+        }
+        _radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        int x = 10;
-        if (x == 10)
-        {
-            Console.WriteLine("X is 10");
-        }
-        for (int i = 0; i < x; i++)
-        {
-            Console.WriteLine($"Logan is cool: {i * i}.");
-        }
+        Circle myCircle = new Circle();
+        Circle myCircle2 = new Circle();
+        myCircle.SetRadius(10);
+        Console.WriteLine($"{myCircle.GetRadius()}");
+
+        myCircle2.SetRadius(20);
+        Console.WriteLine($"{myCircle2.GetRadius()}");
+
+        Console.WriteLine($"{myCircle.GetArea()}");
+        Console.WriteLine($"{myCircle2.GetArea()}");
+        // Console.WriteLine("Hello Sandbox World!");
+        // int x = 10;
+        // if (x == 10)
+        // {
+        //     Console.WriteLine("X is 10");
+        // }
+        // for (int i = 0; i < x; i++)
+        // {
+        //     Console.WriteLine($"Logan is cool: {i * i}.");
+        // }
     }
 }
